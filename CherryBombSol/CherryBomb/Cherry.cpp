2873aab1eb49419c61700f0d86467cb5e864cf3f
unknown <elenich98@gmail.com>
2020-12-15 18:13:53 +0200

    added cherry

--- CherryBombSol/CherryBomb/Cherry.cpp
+++ CherryBombSol/CherryBomb/Cherry.cpp
@@ -2,3 +2,23 @@
 #include "Cherry.h"
 #include "config.h"
 
+void Cherry::update()
+{
+}
+
+void Cherry::draw()
+{
+}
+
+void Cherry::init()
+{
+}
+
+Cherry::Cherry()
+{
+	init();
+}
+
+Cherry::~Cherry()
+{
+}
--- CherryBombSol/CherryBomb/Cherry.h
+++ CherryBombSol/CherryBomb/Cherry.h
@@ -3,3 +3,17 @@
 #include "Object.h"
 #include "config.h"
 
+class Cherry :public Object
+{
+	float pos_x = CANVAS_WIDTH / 2, pos_y = 9 * CANVAS_HEIGHT / 10, speed = 80.f;
+public:
+	void update()override;
+
+	void draw() override;
+
+	void init() override;
+
+	Cherry();
+
+	~Cherry();
+};
